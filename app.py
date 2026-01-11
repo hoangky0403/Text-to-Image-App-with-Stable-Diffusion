1860d8df3d9385cbf9d4585909c7a640e6df7cb7
HoangKy28 <90546210+hoangky0403@users.noreply.github.com>
2026-01-11 07:27:43 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+import tkinter as tk 
+import customtkinter as ctk 
+
+from PIL import ImageTk
+from authtoken import auth_token
+
+import torch
+from torch import autocast
+from diffusers import StableDiffusionPipeline
+
+# create the app
+app = tk.Tk()
+app.geometry("532x632")
+app.title('Stable Bud')
+ctk.set_appearance_mode('dark')
+
+prompt = ctk.CTkEntry(master= app, height = 40, width = 512, font =('Arial', 20), text_color = 'black', fg_color = 'white')
+prompt.place(x=10, y=10)
+
+
+lmain = ctk.CTkLabel(master= app, height = 512, width = 512)
+lmain.place(x=10, y=110)
+
+trigger = ctk.CTkButton(master=app, height = 40, width= 120, font =('Arial', 20), text_color = 'white', fg_color = 'blue', command=lambda: generate())
+trigger.configure(text="Generate")
+trigger.place(x=206, y=60)
+
+device = 'cpu'
+modelid = "CompVis/stable-diffusion-v1-4"
+pipe = StableDiffusionPipeline.from_pretrained(modelid, use_auth_token = auth_token, low_cpu_mem_usage=True)
+pipe.to(device)
+
+
+def generate():
+    
+    image = pipe(prompt.get(), guidance_scale=8.5).images[0]
+
+    image.save('generated_image.png')
+    img = ImageTk.PhotoImage(image)
+  
+    lmain.configure(image=img)
+    lmain.image = img
+
+app.mainloop()
